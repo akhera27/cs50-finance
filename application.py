f5fd4a50eadb9a66396aad7256fb54bf98eefcbf
Aryan Khera <49528890+aryankhera@users.noreply.github.com>
2019-11-10 17:17:48 +0530

    secret key and session_file_dir removed

--- application.py
+++ application.py
@@ -27,10 +27,9 @@ def after_request(response):
 app.jinja_env.filters["usd"] = usd
 
 # Configure session to use filesystem (instead of signed cookies)
-app.config["SESSION_FILE_DIR"] = mkdtemp()
+# app.config["SESSION_FILE_DIR"] = mkdtemp()
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
-app.config["SECRET_KEY"] = '\xfd{H\xe5<\x95\xf9\xe3\x96.5\xd1\x01O<!\xd5\xa2\xa0\x9fR"\xa1\xa8'
 Session(app)
 
 # Configure CS50 Library to use SQLite database
